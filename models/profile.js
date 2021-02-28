29e102c7c159f2d9ddf4cec4313bc70245394815
N-Shar-ma <bablinaneh@gmail.com>
2021-02-28 15:57:51 +0530

    profile names should be unique

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,8 @@ const User = require("./user")
 const profileSchema = new mongoose.Schema({
     name : {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     accounts : {
         Twitter : {
